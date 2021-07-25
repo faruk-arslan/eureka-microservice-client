fdedb46821f5ae1d4ae9066fe704422af40905ce
Faruk Arslan <farukarslan011@gmail.com>
2021-07-25 22:33:04 +0300

    Register as Eureka Client (Microservice).

--- src/main/java/com/fa/dogresteurekamicroservice/DogRestEurekaMicroserviceApplication.java
+++ src/main/java/com/fa/dogresteurekamicroservice/DogRestEurekaMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.fa.dogresteurekamicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DogRestEurekaMicroserviceApplication {
 
 	public static void main(String[] args) {
